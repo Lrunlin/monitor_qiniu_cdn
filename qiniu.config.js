5376a2c4614e6f904cd0cef01c47e7d4eee6692a
LRunLin <liurl0621@gmail.com>
2022-03-28 10:24:53 +0800

    完成
    修改readme，修改config注释

--- qiniu.config.js
+++ qiniu.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    // 开发者Key:https: //portal.qiniu.com/user/key
-    accessKey: 'ByJewUC8v0CW61w8I5-9MwEoMTakPUeJXOi13JY3',
-    secretKey: '6ASGiKc2KbcNjczko2v27u4SMIeVX_TRz4-mH_fj',
+    // 申请开发者Key:https: //portal.qiniu.com/user/key
+    accessKey: '',
+    secretKey: '',
     //时间粒度  5min ／ hour ／day
     granularity: '5min',
     domains: [{
@@ -15,7 +15,7 @@ module.exports = {
     }],
     //邮箱:系统请求下线域名后会对你所留邮箱发送提示
     email: '353575900@qq.com',
-    // 是否每次请求时打印日志
+    // 是否每次请求时打印日志（默认:false）
     consoleLog: false,
     // 多长时间检测到一次（ms）,默认1000
     //!不得小于20（七牛云限制qps为100）
